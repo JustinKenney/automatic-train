b21c9183b417f2427f49d27c1888d634a4de26dd
Justin Kenney <justinkenney45@gmail.com>
2025-12-11 21:24:02 -0500

    Rending script to accept paths as input, oppossed to hard coding them

--- PowerShell/FindInstallersOnServer.ps1
+++ PowerShell/FindInstallersOnServer.ps1
@@ -1,5 +1,9 @@
 ##This is my first attempt at making a script in-line with Functional Programing style/best practices
 
+param(
+    [Parameter(Mandatory)]
+    [string[]]$NetworkLocations
+)
 function Get-SoftwareInstallers {
     param(
         [Parameter(Mandatory, ValueFromPipeline)]
@@ -13,7 +17,6 @@ function Get-SoftwareInstallers {
     }
 }
 
-$NetworkLocations = "\\testpath\folder1", "\\testpath\folder2"
 $InstallerTypes = "*.msi", "*.exe"
 
 $NetworkLocations | Get-SoftwareInstallers -FileType $InstallerTypes | Out-GridView
