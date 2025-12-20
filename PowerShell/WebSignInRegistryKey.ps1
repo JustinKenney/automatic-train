8cdc66a7e1f4207eeca411513f584d6a7143e5b6
Justin Kenney <justinkenney45@gmail.com>
2025-12-19 22:12:27 -0500

    Rewrote script to make use of functions and idempotency

--- PowerShell/WebSignInRegistryKey.ps1
+++ PowerShell/WebSignInRegistryKey.ps1
@@ -1,27 +1,51 @@
 #A basic script that adds a registry key to enable web sign-in on Windows (allows for signing in with modern authentication methods)
 #Note: script must be run as admin
 
-$RegistryPath = "HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\"
-$KeyName = "Authentication"
-$ValueName = "EnableWebSignIn"
-$ValueType = "DWord"
-$ValueContents = "1"
+function Set-RegistryKey {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string]$RegistryPath,
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string]$KeyName,
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string]$ValueName,
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string]$ValueType,
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string]$ValueContents
+    )
 
-$FullKeyPath = Join-Path -Path $RegistryPath -ChildPath $KeyName
+    process {
+        $FullKeyPath = Join-Path -Path $RegistryPath -ChildPath $KeyName
 
-Try {
-    New-Item -Path $RegistryPath -Name $KeyName -Force -ErrorAction Stop  | Out-Null
-} Catch {
-    Write-Host "Could not create registry key!"
-    Write-Host "Error: $($_.Exception.Message)"
-    Exit 1
+        try {
+            if($false -eq (Test-Path -Path $FullKeyPath)) {
+                New-Item -Path $RegistryPath -Name $KeyName -Force -ErrorAction Stop
+            }
+        }
+        catch {
+            Write-Host "Registry key does not exist and could not be created"
+        }
+
+        try{
+            if($ValueContents -ne (Get-ItemPropertyValue -Path $FullKeyPath -Name $ValueName)) {
+                Set-ItemProperty -Path $FullKeyPath -Name $ValueName -Value $ValueContents -Type $ValueType -Force -ErrorAction Stop
+            }
+        }
+        Catch {
+            Write-Host "Could not set registry value"
+        }
+    }
+    
 }
 
-Try {
-    Set-ItemProperty -Path $FullKeyPath -Name $ValueName -Value $ValueContents -Type $ValueType -Force -ErrorAction Stop | Out-Null
-    Write-Host "Web sign in enabled!"
-} Catch {
-    Write-Host "Could not create registry value!"
-    Write-Host "Error: $($_.Exception.Message)"
-    Exit 1
+$params = @{
+    RegistryPath = "HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\"
+    KeyName = "Authentication"
+    ValueName = "EnableWebSignIn"
+    ValueType = "DWord"
+    ValueContents = "1"
 }
+
+Set-RegistryKey @params
